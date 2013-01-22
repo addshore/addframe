0d50834cc07db5827c9e94af2700d0bb8fa4e39d
addshore <a>
2013-01-22 04:16:05 +0000

    Add login
    All config items are in the array $config e.g. $config['user']
    Moving login.php content to run.php (no need to have seperate files)

--- config.gen.php
+++ config.gen.php
@@ -1,12 +1,9 @@
 <?
 
-// bot username
-$user = 'Addbot';
-// bot password as $password = 'password';
-require '/home/addshore/.password.addbot';
-// bot owner
-$owner = 'Addshore';
-// sandbox location
-$sandbox = 'User:'.$owner.'/sandbox';
+$config['url'] = 'en.wikipedia.org';                        // wiki url we will be working on
+$config['user'] = 'Addbot';                                 // bot username for login
+$config['owner'] = 'Addshore';                              // bot owner
+$config['sandbox'] = 'User:'.$config['user'].'/Sandbox';    // sandbox location
+require '/home/addshore/.password.addbot';                  // $config['password'] = 'password';
 
 ?>
--- run.php
+++ run.php
@@ -1,3 +1,21 @@
 <?
 
+// load the classes
+require 'botclasses.php';
+require 'database.php';
+require 'page.php';
+
+// load the configs
+require 'config.gen.php';
+require 'config.regex.php';
+
+// initialise the wiki
+$wiki = new wikipedia;
+$wiki->url = 'http://'.$config['url'].'/w/api.php';
+global $wiki;
+
+// perform the login
+$wiki->login($config['user'],$config['password']);
+unset($config['password']);
+
 ?>
