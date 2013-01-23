1bd190aadbf77f4caaf53d68f19579bda5d0a88c
Adam Shorland <adamshorland@gmail.com>
2013-01-23 22:19:18 +0000

    page.php added parseNamespace()

--- page.php
+++ page.php
@@ -7,20 +7,31 @@ class Page {
 	// construct the page (you probably want to call load after this)
 	public function __construct($page) {
 		$this->page = $page;
-	}
+		parseNamespace();
+	}	
 	
 	// variables
 	private $page;// page name (e.g. "User:Addshore")
 	private $text;// page text
+	private $namespace;// page namespace (No colon)
 	private $parser;// instance of the parser.php class
 	
 	// getters and setters
 	public string getName() { return $page; }
-	public string getText() { return $text;} //TODO: if empty get text
+	public string getText() { if(!isset($text)){loadText();} return $text;}
+	public string getNamespace() { if(!isset($namespace)){parseNamespace();} return $namespace;}
 	
-	// functions
-	public void loadText() { $this->text = $wiki->getpage($this->text);} // load the text from the wiki
+	// public functions
 	public void parse() { $parser = new parser($page,getText()); $parser->parse();} // create instance of parser class and parse
+	
+	// private function
+	private void loadText() { $this->text = $wiki->getpage($page);} // load the text from the wiki
+	private void parseNamespace()
+	{
+		preg_match("/^((User|Wikipedia|File|Image|Mediawiki|Template|Help|Category|Portal|Book|Education( |_)program|TimedText)(( |_)talk)?):?/i",$page,$matches);
+		$this->namespace = $matches[1];
+	}
+	
 	//removeTag($tag,$arguments)
 	//addMultipleissues()
 
--- run.php
+++ run.php
@@ -26,8 +26,8 @@ $result = $db->select('pending','*');
 $list = Database::mysql2array($result);
 foreach ($list as $item)
 {
-	// $item['page'] is the name of the article in this case
-	$page = new Page($item['page']);// create our page instance
+	$page = new Page($item['article']);// create our page instance
+	$page->parse();// parse the page
 }
 
 ?>
