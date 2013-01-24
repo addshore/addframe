a7f7d2f3a1a60fa58813b0c1a7fd356621bccbb8
Adam Shorland <adamshorland@gmail.com>
2013-01-24 02:34:10 +0000

    start maintain.php
    This will serve as an hourly maintanace script to be run

---
+++ maintain.php
@@ -0,0 +1,11 @@
+<?
+require 'database.php';
+require 'config.php';
+
+echo "Connecting to DB...\n";
+$db = new Database( $config['dbhost'], $config['dbport'], $config['dbuser'], $config['dbpass'], $config['dbname'], false);
+
+// remove checked articles older than 24 hours
+$res = $db->doQuery("DELETE FROM checked WHERE NOW()-24 > checked");
+
+?>
