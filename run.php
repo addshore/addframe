7add588f19e0b28fbfa94f7274cdd79d7ef9cda9
Adam Shorland <adamshorland@gmail.com>
2013-02-02 17:28:16 +0000

    process_sections true for parseini

--- run.php
+++ run.php
@@ -25,8 +25,7 @@ unset($config['password']);
 echo "done";
 
 //Get further config stuff
-//eval(preg_replace("/(\<syntaxhighlight lang='php'\>|\<\/syntaxhighlight\>)/i","",$wiki->getpage("User:Addbot/config")));//run the onwiki config
-$config = parse_ini_string(file_get_contents(preg_replace("/(\<syntaxhighlight lang='php'\>|\<\/syntaxhighlight\>)/i","",$wiki->getpage("User:Addbot/config"))));
+$config = parse_ini_string(file_get_contents(preg_replace("/(\<syntaxhighlight lang='php'\>|\<\/syntaxhighlight\>)/i","",$wiki->getpage("User:Addbot/config"))),true);
 if($config['run'] != true){echo "\nNot set to run"; die();}//if we are not meant to run die
 
 // connect to the database
