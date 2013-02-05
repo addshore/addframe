20d73825ae8756c484fbceb8b70ac3a8f24be330
Adam Shorland <adamshorland@gmail.com>
2013-02-05 16:30:38 +0000

    Alter regex Fixes #57

--- classes/page.php
+++ classes/page.php
@@ -271,7 +271,7 @@ class Page {
 			foreach($cats as $cat)
 			{
 				echo $cat;
-				if(!preg_match('/^Category:(Proposed deletion as of .*?|(|.*? )stubs$)/i',$cat))
+				if(!preg_match('/^Category:(.*?Proposed (for )?deletion.*?|(|.*? )stubs$)/i',$cat))
 				{
 					//if it is not a stub cat return FALSE (NOT UNCAT)
 					return false;
