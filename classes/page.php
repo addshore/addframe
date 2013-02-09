d50e1cf85eee5753a897383a3defba2c923cc783
Adam Shorland <adamshorland@gmail.com>
2013-02-09 15:12:14 +0000

    rm line that caused errors

--- classes/page.php
+++ classes/page.php
@@ -669,8 +669,6 @@ class Page {
 			//if the tag can be found without a date
 			if($this->matches('/\{\{(Template:)?'.$tag->regexName().'/i'))
 			{
-				//name to the main template
-				$text = preg_replace('/\{\{(Template:)?'.$tag->regexName().'/i',"{{".$tag->getName(),$text);
 				//then date it
 				$text = preg_replace('/\{\{(Template:)?'.$tag->regexName().'\}\}/i',"{{".$tag->getName()."|date=$date}}",$text);
 			}
