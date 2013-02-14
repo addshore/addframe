04199fbeb9d632aa345c040aef98e84b1d8d2e4a
Adam Shorland <adamshorland@gmail.com>
2013-02-14 13:18:15 +0000

    fix Distinguish hatnote

--- classes/page.php
+++ classes/page.php
@@ -500,7 +500,7 @@ class Page {
 					$mi = preg_replace("/\n/","",$mi);//get rid of new lines
 				}
 				//else do we match any hatnotes
-				elseif(preg_match('/^(Template:)?(Hatnote|Reflink|Main(( |_)list)?|Details3?|See( |_)also2?|Further2?|About|Other( |_)uses-section|For|((Two|Three) )?Other( |_)uses|Other uses of|Redirect[0-1]?[0-9]|Redirect(-|_| )(synomym|text|distinguish2?)|Consider( |_)disambiguation|Other( |_)(uses|people|places|hurricanes|ships|)[1-5]?|(Redirect-)Distinguish|Selfref|Category( |_)(see also|explanation|pair)|Cat( |_)main|cat(preceding|succeeding)|contrast|This( |_)user( |_)talk)/i',$x->name))
+				elseif(preg_match('/^(Template:)?(Hatnote|Reflink|Main(( |_)list)?|Details3?|See( |_)also2?|Further2?|About|Other( |_)uses-section|For|((Two|Three) )?Other( |_)uses|Other uses of|Redirect[0-1]?[0-9]|Redirect(-|_| )(synomym|text|distinguish2?)|Consider( |_)disambiguation|Other( |_)(uses|people|places|hurricanes|ships|)[1-5]?|(Redirect-)?Distinguish|Selfref|Category( |_)(see also|explanation|pair)|Cat( |_)main|cat(preceding|succeeding)|contrast|This( |_)user( |_)talk)/i',$x->name))
 				{
 					//make sure the hat note is not under a section
 					if(!preg_match('/\n==.*?{{'.$x->name.'/is',$this->getText()))
