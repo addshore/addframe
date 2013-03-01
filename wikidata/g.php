fcbacbfe772c4aac5f04a0be1f8820caa6575d60
Adam Shorland <adamshorland@gmail.com>
2013-03-01 21:38:24 +0000

    alignment and regex alter

--- wikidata/g.php
+++ wikidata/g.php
@@ -39,7 +39,7 @@ if(!isset($file))
 	$db = new Database( $config['dbhost'], $config['dbport'], $config['dbuser'], $config['dbpass'], $config['dbname'], false);
 	echo "done";
 
-	$result = $db->select('iwlinked','*',"lang = '$glang'",array("LIMIT" => 200));
+	$result = $db->select('iwlinked','*',"lang = '$glang'",array("LIMIT" => 150));
 	$list = Database::mysql2array($result);
 	echo "\nGot ".count($list)." articles from $glang pending";
 	echo "\nRemoving";
@@ -68,53 +68,48 @@ foreach ($list as $item)
 	echo ".";
 	$text = $wiki->getpage($name,null,true);
 	if (strlen($text) < 2){echo "-";continue;}
-	
-	//$result = preg_match("/^((Talk|User|Wikipedia|File|Image|Mediawiki|Template|Help|Category|Portal|Book|Education( |_)program|TimedText)(( |_)talk)?):?/i",$name,$matches);
-	//if($result == 0)
-	//{
-				$r = $wiki->wikidatasitelinks($name,$glang);
-				$counter = 0;
-				$id = "";
-				
-				if(count($r) == 1)
+		$r = $wiki->wikidatasitelinks($name,$glang);
+		$counter = 0;
+		$id = "";
+		
+		if(count($r) == 1)
+		{
+			foreach($r as $ent)
+			{
+				$id = $ent['id'];
+				if(isset($ent['sitelinks']))
 				{
-					foreach($r as $ent)
+					foreach ($ent['sitelinks'] as $l)
 					{
-						$id = $ent['id'];
-						if(isset($ent['sitelinks']))
+						$lang = str_replace("_","-",str_replace("wiki","",$l['site']));
+						$link = "\n ?[[".$lang." ?: ?".$l['title']." ?]] ?";
+						$link = str_replace(" ","( |_)",preg_quote($link,'/'));
+						if(preg_match('/'.$link.'/',$text))
 						{
-							foreach ($ent['sitelinks'] as $l)
-							{
-								$lang = str_replace("_","-",str_replace("wiki","",$l['site']));
-								$link = "\n[[".$lang.":".$l['title']."]]";
-								$link = str_replace(" ","( |_)",preg_quote($link,'/'));
-								if(preg_match('/'.$link.'/',$text))
-								{
-									$text = preg_replace('/'.$link.'/',"",$text);//remove the link
-									$counter++;
-								}
-							}
+							$text = preg_replace('/'.$link.'/',"",$text);//remove the link
+							$counter++;
 						}
 					}
-					if($counter > 0)
-					{
-					switch($glang) {
-						case "en":$summary = "[[User:Addbot|Bot:]] Migrating $counter interwiki links, now provided by [[Wikipedia:Wikidata|Wikidata]] on [[d:$id]]"; break;
-						case "it":$summary = "migrazione di $counter interwiki links su [[d:Wikidata:Pagina_principale|Wikidata]] - [[d:$id]]"; break;
-						case "he":$summary = "בוט: מעביר קישורי בינויקי ל[[ויקיפדיה:ויקינתונים|ויקינתונים]] - [[d:$id]]"; break;
-						case "nl":$summary = "Verplaatsing van $counter interwikilinks die op [[d:|Wikidata]] beschikbaar zijn op [[d:$id]]"; break;
-						case "sv":$summary = "[[User:Addbot|Bot]] överför $counter interwikilänk(ar), som nu återfinns på sidan [[d:$id]] på [[Wikipedia:Wikidata|Wikidata]]"; break;
-						default:$summary = "[[User:Addbot|Bot:]] Migrating $counter interwiki links, now provided by [[Wikipedia:Wikidata|Wikidata]] on [[d:$id]]";
-					}
-						
-					}
-				}			
+				}
+			}
+			if($counter > 0)
+			{
+			switch($glang) {
+				case "en":$summary = "[[User:Addbot|Bot:]] Migrating $counter interwiki links, now provided by [[Wikipedia:Wikidata|Wikidata]] on [[d:$id]]"; break;
+				case "it":$summary = "migrazione di $counter interwiki links su [[d:Wikidata:Pagina_principale|Wikidata]] - [[d:$id]]"; break;
+				case "he":$summary = "בוט: מעביר קישורי בינויקי ל[[ויקיפדיה:ויקינתונים|ויקינתונים]] - [[d:$id]]"; break;
+				case "nl":$summary = "Verplaatsing van $counter interwikilinks die op [[d:|Wikidata]] beschikbaar zijn op [[d:$id]]"; break;
+				case "sv":$summary = "[[User:Addbot|Bot]] överför $counter interwikilänk(ar), som nu återfinns på sidan [[d:$id]] på [[Wikipedia:Wikidata|Wikidata]]"; break;
+				default:$summary = "[[User:Addbot|Bot:]] Migrating $counter interwiki links, now provided by [[Wikipedia:Wikidata|Wikidata]] on [[d:$id]]";
+				}
 				
-			$text = preg_replace("/<!-- ?interwikis?( links?)? ?-->/i","",$text);
-				
-			$text = preg_replace('/(\n\n)\n+/',"$1", $text);
-			$text = preg_replace('/^(\n|\r){0,5}/',"", $text );
-	//}
+			}
+		}			
+			
+		$text = preg_replace("/<!-- ?interwikis?( links?)? ?-->/i","",$text);
+			
+		$text = preg_replace('/(\n\n)\n+$/',"$1", $text);
+		$text = preg_replace('/^(\n|\r){0,5}$/',"", $text );
 	
 	if($summary != "")
 	{
