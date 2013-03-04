24030094d5608ffc3bf93131e22676706baaf87e
Adam Shorland <adamshorland@gmail.com>
2013-03-04 13:56:44 +0000

    create central configs

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?
+// database settings
+$config['dbhost'] = 'bots-bsql01';
+$config['dbport'] = '3306';
+$config['dbuser'] = 'addshore';
+require '/home/addshore/.password.db';            //$config['dbpass'] = 'password';
+$config['dbname'] = 'addbot';
+?>
---
+++ config/wiki.php
@@ -0,0 +1,8 @@
+<?
+
+$config['user'] = 'Addbot';                                 // bot username for login
+$config['owner'] = 'Addshore';                              // bot owner
+$config['mysandbox'] = 'User:'.$config['user'].'/Sandbox';    // sandbox location
+require '/home/addshore/.password.addbot';                  // $config['password'] = 'password';
+
+?>
