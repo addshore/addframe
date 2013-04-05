39829852d11f2445f56e8bc5cfcd4a930320711a
Adam Shorland <adamshorland@gmail.com>
2013-04-05 01:47:17 +0100

    change agent

--- classes/botclasses.php
+++ classes/botclasses.php
@@ -75,7 +75,7 @@ class http {
         //echo 'POST: '.$url."\n";
         $time = microtime(1);
         curl_setopt($this->ch,CURLOPT_URL,$url);
-        curl_setopt($this->ch,CURLOPT_USERAGENT,'php wikibot classes');
+        curl_setopt($this->ch,CURLOPT_USERAGENT,'Addbot Wikimedia Bot');
         /* Crappy hack to add extra cookies, should be cleaned up */
         $cookies = null;
         foreach ($this->cookie_jar as $name => $value) {
