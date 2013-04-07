38051df4924c3e246ea1c15b9437193622c6664c
Adam Shorland <adamshorland@gmail.com>
2013-04-07 09:03:28 +0100

    reverse the move

--- wikidata/dbmove.php
+++ wikidata/dbmove.php
@@ -6,43 +6,37 @@ require '/data/project/addbot/classes/stathat.php';
 require '/data/project/addbot/config/stathat.php';
 $db = new Database( $config['dbhost'], $config['dbport'], $config['dbuser'], $config['dbpass'], $config['dbname'], false);
 unset($config['dbpass']);
-
 $c = 0;
 
-$q = "SELECT MAX(id) as max_id, MIN(id) as min_id FROM iwlinked;";
-echo "$q\n";
-$r = Database::mysql2array($db->doQuery($q));
-$min = $r[0]['min_id'];
-$max = $r[0]['max_id'];
-$rnd = mt_rand($min, $max);
-
-while($c < 10000)
+while($c < 100)
 {
-	$q2 = "INSERT INTO iwlink (lang, article, links) VALUES ";
-	$q1 = "DELETE FROM iwlinked WHERE id IN (";
 
 	$c++;
-	$q0 = "SELECT * FROM iwlinked WHERE id<=$rnd ORDER BY id DESC LIMIT 20;";
+	$q0 = "SELECT * FROM iwlink LIMIT 20";
 	echo "$q0\n";
 	$r = Database::mysql2array($db->doQuery($q0));
+	sleep(1);
 	foreach($r as $row)
 	{
-		$id = $row['id'];
 		$lang = $row['lang'];
 		$article = $row['article'];
 		$links = $row['links'];
 		if($links == ""){$links = "NULL";}
-		$q1 .= "$id, ";
-		$q2 .= "('$lang', '$article', $links), ";
+		$q = "INSERT DELAYED INTO iwlinked (lang, article, links) VALUES ('$lang', '$article', '$links')";
+		echo "$q\n";
+		$r = $db->doQuery($q);
+		if( !$r  )
+		{
+			echo $db->errorStr()."\n";
+		}
+		else
+		{
+			$q = "DELETE FROM iwlink WHERE ( lang='$lang' AND article='$article')";
+			echo "$q\n";
+			$r = $db->doQuery($q);
+			if( !$r  ){echo $db->errorStr()."\n";}
+		}
 	}
-	$q1 = trim($q1,', ').");";
-	$q2 = trim($q2,', ').";";
-	echo "$q1\n";
-	$r1 = $db->doQuery($q1);
-	if( !$r1  ){echo $db->errorStr();}
-	echo "$q2\n";
-	$r2 = $db->doQuery($q2);
-	if( !$r2  ){echo $db->errorStr();}
 	
 }
 
