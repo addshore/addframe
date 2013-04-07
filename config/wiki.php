de8f403ed5ebeb0d063383b8ed388948ff97f639
Adam Shorland <adamshorland@gmail.com>
2013-04-07 09:03:46 +0100

    config

--- config/wiki.php
+++ config/wiki.php
@@ -3,6 +3,6 @@
 $config['user'] = 'Addbot';                                 // bot username for login
 $config['owner'] = 'Addshore';                              // bot owner
 $config['mysandbox'] = 'User:'.$config['user'].'/Sandbox';    // sandbox location
-require '/home/addshore/.password.addbot';                  // $config['password'] = 'password';
+require '/mnt/secure/addshore/.password.addbot';                  // $config['password'] = 'password';
 
 ?>
