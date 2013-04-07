8047cc698c7a04f92a88c23f4f519ea14c6e3325
Adam Shorland <adamshorland@gmail.com>
2013-04-07 09:59:33 +0100

    change the way we delete

--- wikidata/iwlinked_del.php
+++ wikidata/iwlinked_del.php
@@ -6,26 +6,22 @@ require '/data/project/addbot/config/database.php';
 $db = new Database( $config['dbhost'], $config['dbport'], $config['dbuser'], $config['dbpass'], $config['dbname'], false);
 unset($config['dbpass']);
 
-$list = Database::mysql2array($db->doQuery('SELECT * from iwlinked_del ORDER BY added ASC LIMIT 100'));
+$toget = 100;
 
-echo "\nSelected ".count($list)." deletions\n";
-
-//Remove the from the del table first
-foreach ($list as $item)
+echo "\nSelecting $toget deletions\n";
+$c = 0;
+while($c<$toget)
 {
-$q = "DELETE FROM iwlinked_del WHERE (lang='".$db->mysqlEscape($item['lang'])."' AND article='".$db->mysqlEscape($item['article'])."')";
-echo "\n$q";
-$res = $db->doQuery($q);
-if( !$res  ){echo "\n".$db->errorStr();}
-}
-
-//and then remove them from the main table
-foreach ($list as $item)
-{
-$q = "DELETE FROM iwlinked WHERE (lang='".$db->mysqlEscape($item['lang'])."' AND article='".$db->mysqlEscape($item['article'])."')";
-echo "\n$q";
-$res = $db->doQuery($q);
-if( !$res  ){echo "\n".$db->errorStr();}
+	$c++;
+	$item = Database::mysql2array($db->doQuery('SELECT * from iwlinked_del ORDER BY added ASC LIMIT 1 OFFSET '.rand(0,10)));
+	$q = "DELETE FROM iwlinked_del WHERE (lang='".$db->mysqlEscape($item[0]['lang'])."' AND article='".$db->mysqlEscape($item[0]['article'])."')";
+	echo "\n$q";
+	$res = $db->doQuery($q);
+	if( !$res  ){echo "\n".$db->errorStr();}
+	$q = "DELETE FROM iwlinked WHERE (lang='".$db->mysqlEscape($item[0]['lang'])."' AND article='".$db->mysqlEscape($item[0]['article'])."')";
+	echo "\n$q";
+	$res = $db->doQuery($q);
+	if( !$res  ){echo "\n".$db->errorStr();}
 }
 
 ?>
