e0d2f16ed45662b7fc2939a298114122a9aa45d5
addshore-dev <local-addshore-dev@tools.wmflabs.org>
2013-05-19 16:22:08 +0000

    script made for cs.wikinews to delete all in a cat

---
+++ scripts/cs.wikinews-catempty.php
@@ -0,0 +1,23 @@
+<?
+//Classes and configs
+require '/data/project/addbot/classes/botclasses.php';
+require '/data/project/addbot/config/wiki.php';
+
+//and wikidata
+$wiki      = new wikipedia;
+$wiki->url = "http://cs.wikinews.org/w/api.php";
+global $wiki;
+echo "\nLogging in to cs.wikinews.org...";
+$wiki->login("User", "Password");
+
+$reason = "Nahrazeno nominativní kategorií";
+
+$pages = $wiki->categorymembers("Kategorie:Podle dne");
+echo count($pages)."\n";
+foreach($pages as $page)
+{
+	echo "$page\n";
+	//Uncomment the line below to actually perform the operation
+	//$wiki->delete($page,$reason);
+}
+?>
