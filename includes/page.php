16b6ae6390e2f16f900b66a12eb70da200c527da
addshore <addshorewiki@gmail.com>
2013-07-07 22:34:45 +0200

    string functions, further planning

--- includes/page.php
+++ includes/page.php
@@ -30,8 +30,14 @@ class Page {
 	 * @var string timestamp for the particular revision text we have got
 	 */
 	public $timestamp;
-
+	/**
+	 * @var array of categories the page is in
+	 */
 	public $categories;
+	/**
+	 * @var string current protection status
+	 */
+	public $protection;
 
 	function __construct( $handel , $title ) {
 		$this->handel = $handel;
@@ -53,6 +59,13 @@ class Page {
 			$this->timestamp = $x['revisions']['0']['timestamp'];
 			return $this->text;
 		}
+	}
+
+	function getProtectionStatus(){
+		//@todo write this
+		////http://en.wikipedia.org/w/api.php?action=query&titles=Cyprus&prop=info&inprop=protection
+		//checkout https://github.com/addshore/addwiki/blob/b8db6c00049d6ff2cefe92187e744c4c6693f815/classes/botclasses.php ln1083
+		//return $this->protection;
 	}
 
 	/**
@@ -109,13 +122,38 @@ class Page {
 	}
 
 	/**
+	 * Find a string
+	 * @param $string The string that you want to find.
+	 * @return bool value (1 found and 0 not-found)
+	 **/
+	function findString( $string ){
+		if( strstr( $this->text, $string ) )
+			return 1;
+		else
+			return 0;
+	}
+
+	/**
+	 * Replace a string
+	 * @param $string The string that you want to replace.
+	 * @param $newstring The string that will replace the present string.
+	 * @return the new text of page
+	 **/
+	function replaceString( $string, $newstring ){
+		$this->text = str_replace( $string, $newstring, $this->text );
+		return $this->text;
+	}
+
+	/**
 	 * gets array of interwiki links from text
 	 * @from https://github.com/addshore/addwiki/blob/b8db6c00049d6ff2cefe92187e744c4c6693f815/bot/wikidata/g.php ln140
 	 * @return array of interwikilinks matches in text
+	 * @author addshore
 	 */
 	function getInterwikiLinksFromText()
 	{
 		$return = Array();
+		//@todo this langcode should be fetched on execution and be configurable from somewhere (not static here)
 		preg_match_all("/\[\[(nostalgia|ten|aa|ab|ace|af|ak|als|am|an|ang|ar|arc|arz|as|ast|av|ay|az|ba|bar|bat-smg|bcl|be|be-x-old|bg|bh|bi|bjn|bm|bn|bo|bpy|br|bs|bug|bxr|ca|cbk-zam|cdo|ce|ceb|ch|cho|chr|chy|ckb|co|cr|crh|cs|csb|cu|cv|cy|da|de|diq|dsb|dv|dz|ee|el|eml|en|eo|es|et|eu|ext|fa|ff|fi|fiu-vro|fj|fo|fr|frp|frr|fur|fy|ga|gag|gan|gd|gl|glk|gn|got|gu|gv|ha|hak|haw|he|hi|hif|ho|hr|hsb|ht|hu|hy|hz|ia|id|ie|ig|ii|ik|ilo|io|is|it|iu|ja|jbo|jv|ka|kaa|kab|kbd|kg|ki|kj|kk|kl|km|kn|ko|koi|kr|krc|ks|ksh|ku|kv|kw|ky|la|lad|lb|lbe|lez|lg|li|lij|lmo|ln|lo|lt|ltg|lv|map-bms|mdf|mg|mh|mhr|mi|min|mk|ml|mn|mo|mr|mrj|ms|mt|mus|mwl|my|myv|mzn|na|nah|nap|nds|nds-nl|ne|new|ng|nl|nn|no|nov|nrm|nso|nv|ny|oc|om|or|os|pa|pag|pam|pap|pcd|pdc|pfl|pi|pih|pl|pms|pnb|pnt|ps|pt|qu|rm|rmy|rn|ro|roa-rup|roa-tara|ru|rue|rw|sa|sah|sc|scn|sco|sd|se|sg|sh|si|simple|sk|sl|sm|sn|so|sq|sr|srn|ss|st|stq|su|sv|sw|szl|ta|te|tet|tg|th|ti|tk|tl|tn|to|tpi|tr|ts|tt|tum|tw|ty|udm|ug|uk|ur|ve|vec|vep|vi|vls|vo|wa|war|wo|wuu|xal|xh|xmf|yi|yo|za|zea|zh|zh-classical|zh-min-nan|zh-yue|zu):([^\]]+)\]\]/i",$this->text,$match);
 		//$match[0] is whole match [1] is langcode [2] is pagename
 		foreach($match[0] as $key => $m)
@@ -124,5 +162,16 @@ class Page {
 			$return[$match[1][$key]] = $match[2][$key];
 		}
 		return $return;
+	}
+
+
+	/**
+	 * Gets an entity from this page
+	 * @return WikibaseEntity
+	 */
+	function getEntityFromPage()
+	{
+		//@todo this can only work if we also know the pages language / sitelink code (could we use handel?)
+		//return new WikibaseEntity('')
 	}
 }
