1c7d5b0431305e4f24a7b5630c31eabb8e4b4d63
addshore <addshorewiki@gmail.com>
2013-07-10 19:34:59 +0200

    filenames

--- init.php
+++ init.php
@@ -9,7 +9,19 @@
 $IP = dirname(__FILE__) . '/';
 
 //Include all files in /includes
-foreach (glob("$IP/includes/*.php") as $filename){ include $filename; }
+include $IP . 'includes/Globals.php';
+include $IP . 'includes/General.php';
+include $IP . 'includes/Registry.php';
+include $IP . 'includes/Http.php';
+include $IP . 'includes/Mediawiki.php';
+include $IP . 'includes/MediawikiAPI.php';
+include $IP . 'includes/MediawikiAPIResult.php';
+include $IP . 'includes/SiteFactory.php';
+include $IP . 'includes/User.php';
+include $IP . 'includes/UserLogin.php';
+include $IP . 'includes/Page.php';
+include $IP . 'includes/Category.php';
+include $IP . 'includes/WikibaseEntity.php';
 
 //Factory for creating sites
 Globals::$Sites = new SiteFactory();
