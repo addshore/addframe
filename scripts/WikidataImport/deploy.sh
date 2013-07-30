121b216573fa6823d40c8af29027f952492b6c0a
local-addbot <local-addbot@tools.wmflabs.org>
2013-07-30 09:48:56 +0000

    Fix the deploy script

--- scripts/WikidataImport/deploy.sh
+++ scripts/WikidataImport/deploy.sh
@@ -3,31 +3,31 @@
 #The deploy script for the WikidataImport code
 #
 
-BASEDIR=$(dirname $0)
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
 
-jstart -mem 512m -N WdIm.m php $BASEDIR/master.php
+jstart -mem 512m -N WdIm.m php $ABSPATH/master.php
 
-jstart -mem 512m -N WdIm.01 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.02 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.03 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.04 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.05 php $BASEDIR/slave.php
+jstart -mem 512m -N WdIm.01 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.02 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.03 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.04 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.05 php $ABSPATH/slave.php
 
-jstart -mem 512m -N WdIm.06 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.07 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.08 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.09 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.10 php $BASEDIR/slave.php
+jstart -mem 512m -N WdIm.06 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.07 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.08 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.09 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.10 php $ABSPATH/slave.php
 
 
-jstart -mem 512m -N WdIm.11 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.12 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.13 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.14 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.15 php $BASEDIR/slave.php
+jstart -mem 512m -N WdIm.11 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.12 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.13 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.14 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.15 php $ABSPATH/slave.php
 
-jstart -mem 512m -N WdIm.16 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.17 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.18 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.19 php $BASEDIR/slave.php
-jstart -mem 512m -N WdIm.20 php $BASEDIR/slave.php
+jstart -mem 512m -N WdIm.16 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.17 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.18 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.19 php $ABSPATH/slave.php
+jstart -mem 512m -N WdIm.20 php $ABSPATH/slave.php
