f38fa98657705dabf8191458fec5389d1089be6b
addshore <addshorewiki@gmail.com>
2013-07-31 09:54:14 +0200

    Class ready without threads!

--- includes/IrcServer.php
+++ includes/IrcServer.php
@@ -25,25 +25,18 @@ class IrcServer  {
 
 	private function connect() {
 		$this->socket = @fsockopen($this->host, $this->port, $errno, $errstr, 10);
-		//@tothink @todo The PCNTL extension requires *nix platforms.
-		$pid = \pcntl_fork();
-		if( $pid == 0 ){
-			set_time_limit(0);
+		if( $this->socket ){
+			$this->write( "NICK ".$this->username );
+			$this->write( "USER ".$this->username );
 
-			if( $this->socket ){
-				$this->write( "NICK ".$this->username );
-				$this->write( "USER ".$this->username );
+			while( !feof ( $this->socket ) ){
+				$read = str_replace(array("\n","\r"),'',fgets($this->socket, 1024)); //get a line of data from the server
+				$parts = explode(' ',$read);
 
-				while( !feof ( $this->socket ) ){
-					$read = str_replace(array("\n","\r"),'',fgets($this->socket, 1024)); //get a line of data from the server
-					$parts = explode(' ',$read);
-
-					if (strtolower($parts[0]) == 'ping') {
-						$this->write("PONG :".substr($read, 6)); //Reply with pong
-					} elseif (strtolower($parts[1]) == 'privmsg') {
-						$this->addToReadBuffer( $read );
-					}
-
+				if (strtolower($parts[0]) == 'ping') {
+					$this->write("PONG :".substr($read, 6)); //Reply with pong
+				} elseif (strtolower($parts[1]) == 'privmsg') {
+					$this->addToReadBuffer( $read );
 				}
 			}
 		}
