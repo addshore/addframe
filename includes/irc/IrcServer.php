b95d1158765cf6456e865e321a55c05a6b7f4c8f
addshore <addshorewiki@gmail.com>
2013-08-02 02:06:19 +0200

    even more irc functions

--- includes/irc/IrcServer.php
+++ includes/irc/IrcServer.php
@@ -11,12 +11,13 @@ namespace Addframe;
 
 class IrcServer  {
 
-	private $socket;
-	private $host;
-	private $port;
-	private $username;
+	protected $socket;
+	protected $host;
+	protected $port;
+	protected $username;
+	protected $channels;
 
-	function __construct( $host, $username, $port = 6667, $connect = true ) {
+	public function __construct( $host, $username, $port = 6667, $connect = true ) {
 		$this->host = $host;
 		$this->port = $port;
 		$this->username = $username;
@@ -28,8 +29,8 @@ class IrcServer  {
 	private function connect() {
 		$this->socket = @fsockopen($this->host, $this->port, $errno, $errstr, 10);
 		if( $this->socket ){
-			$this->write( "NICK ".$this->username );
-			$this->write( "USER ".$this->username );
+			$this->doNICK( );
+			$this->doUSER( );
 
 			while( !feof ( $this->socket ) ){
 				$read = str_replace(array("\n","\r"),'',fgets($this->socket, 1024)); //get a line of data from the server
@@ -46,14 +47,14 @@ class IrcServer  {
 		$read = str_replace(array("\n","\r"),'',fgets($this->socket, 1024));
 		$parts = explode(' ',$read);
 		if (strtolower($parts[0]) == 'ping') {
-			$this->pong( $read );
+			$this->doPONG( $read );
 		} elseif (strtolower($parts[1]) == 'privmsg') {
 			return $read;
 		}
 		return $this->read();
 	}
 
-	public function pong( $read ){
+	public function doPONG( $read ){
 		//todo, does this actually need $read?
 		$this->write("PONG :".substr($read, 6));
 	}
@@ -62,9 +63,31 @@ class IrcServer  {
 		return !feof ( $this->socket );
 	}
 
-	public function changeusername( $newUsername ){
-		$this->write("NICK ".$newUsername);
-		$this->username = $newUsername;
+	public function changeNick( $newNick ){
+		$this->username = $newNick;
+		$this->doNICK();
+	}
+
+	private function doNICK( ){
+		$this->write("NICK ".$this->username);
+	}
+
+	private function doUSER( ){
+		$this->write( "USER ".$this->username );
+	}
+
+	private function doQUIT( ){
+		$this->write( "QUIT" );
+	}
+
+	private function doPRIVMSG( $string = '' ){
+		$this->write( "PRIVMSG".$string );
+	}
+
+	public function newChannel ( $channelname ){
+		$channel = new IrcChannel( $this, $channelname );
+		$this->channels[] = $channel;
+		return $channel;
 	}
 
 }
