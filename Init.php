7dce0ab3b2335caebd442ab62b9a0c855042b20b
addshore <addshorewiki@gmail.com>
2013-08-20 11:32:04 +0200

    Fix Innit style issues

--- Init.php
+++ Init.php
@@ -9,10 +9,10 @@ namespace Addframe;
 
 $IP = dirname( __FILE__ ) . '/Includes';
 $CP = dirname( __FILE__ ) . '/Configs';
-require_once("$IP/Addframe.init.php");
-require_once("$IP/Irc/Irc.init.php");
-require_once("$IP/Mediawiki/Mediawiki.init.php");
-require_once("$IP/Mediawiki/Wikibase/Wikibase.init.php");
+require_once( "$IP/Addframe.init.php" );
+require_once( "$IP/Irc/Irc.init.php" );
+require_once( "$IP/Mediawiki/Mediawiki.init.php" );
+require_once( "$IP/Mediawiki/Wikibase/Wikibase.init.php" );
 require_once( "$IP/KLogger/src/KLogger.php" );
 
 Config::loadConfigs( $CP );
