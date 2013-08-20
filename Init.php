0e29ae4872088282df3ff25b7698a5f552ff8fbb
Kunal Mehta <legoktm@gmail.com>
2013-08-19 22:17:19 -0700

    wtf

--- Init.php
+++ Init.php
@@ -13,6 +13,6 @@ require_once("$IP/Addframe.init.php");
 require_once("$IP/Irc/Irc.init.php");
 require_once("$IP/Mediawiki/Mediawiki.init.php");
 require_once("$IP/Mediawiki/Wikibase/Wikibase.init.php");
-require_once( dirname( __FILE__ ) . '/../KLogger/src/Klogger.php' );
+require_once( dirname( __FILE__ ) . '/../../KLogger/src/Klogger.php' );
 
 Config::loadConfigs( $CP );
