9dcb99bbcb81b4900bbe398eccac982586df1e7f
Kunal Mehta <legoktm@gmail.com>
2013-08-21 01:22:15 -0700

    Add test for User::requestEditcount

--- Includes/Mediawiki/Tests/UserTest.php
+++ Includes/Mediawiki/Tests/UserTest.php
@@ -10,7 +10,17 @@ use Addframe\Mediawiki\User;
  * @since 0.0.2
  *
  * @author Addshore
+ * @author Legoktm
  */
+
+/**
+ * So we can hijack and set our own custom info
+ */
+class TestUser extends User {
+	public function setUserInfo( $data ) {
+		$this->userinfo = $data;
+	}
+}
 
 class UserTest extends \PHPUnit_Framework_TestCase {
 
@@ -53,6 +63,26 @@ class UserTest extends \PHPUnit_Framework_TestCase {
 		$userTalkPage = $user->getUserTalkPage();
 		$this->assertInstanceOf( 'Addframe\Mediawiki\Page', $userTalkPage , 'Did not return instance of Page' );
 		$this->assertEquals( 'User talk:'.$username, $userTalkPage->getTitle(), 'Did not set the correct Page Title' );
+	}
+
+	public static function provideRandomNumbers() {
+		$arr = array();
+		while ( count( $arr ) < 50 ) {
+			$arr[] = array( rand(1, 100000000) );
+		}
+		return $arr;
+	}
+
+	/**
+	 * @dataProvider provideRandomNumbers
+	 * @param int $count
+	 */
+	function testRequestEditCount( $count ) {
+		// inject it
+		$user = new TestUser( $this->getMockSite(), 'Username' );
+		$user->setUserInfo( array( 'editcount' => $count ) );
+		$this->assertEquals( $count, $user->requestEditcount() );
+
 	}
 
 }
