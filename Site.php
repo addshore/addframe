128d32876fe507792b49f3e7413b9d2f24d1d298
addshore <addshorewiki@gmail.com>
2013-08-22 19:11:00 +0200

    Site can now have a url

--- Site.php
+++ Site.php
@@ -4,7 +4,22 @@ namespace Addframe\Wiki;
 
 class Site {
 
+	var $url;
+
 	public function __construct() {
+	}
+
+	public function setUrl( $url ){
+		$this->url = $url;
+	}
+
+	public function getUrl(){
+		return $this->url;
+	}
+
+	public static function newFromUrl( $url ){
+		$site = new Site;
+		$site->setUrl( $url );
 	}
 
 }
