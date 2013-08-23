aa6a16a8f31485cdcf4075f9854793b25fa817b8
addshore <addshorewiki@gmail.com>
2013-08-23 12:55:59 +0200

    Sub directory for mediawiki

--- Addframe.classes.php
+++ Addframe.classes.php
@@ -5,9 +5,9 @@ return call_user_func( function() {
 	$classes = array(
 		'Addframe\Http' => 'Http.php',
 
-		'Addframe\Mediawiki\Site' => 'Site.php',
-		'Addframe\Mediawiki\Api' => 'Api.php',
-		'Addframe\Mediawiki\SiteList' => 'SiteList.php',
+		'Addframe\Mediawiki\Site' => 'mediawiki/Site.php',
+		'Addframe\Mediawiki\Api' => 'mediawiki/Api.php',
+		'Addframe\Mediawiki\SiteList' => 'mediawiki/SiteList.php',
 	);
 
 	return $classes;
