d2381ab0e2add89e43a49f6d170f2f3dfc55f307
addshore <addshorewiki@gmail.com>
2013-08-24 20:26:48 +0200

    Everything in Cache now accepts the Cachable obj!

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -11,31 +11,31 @@ class CacheTest extends PHPUnit_Framework_TestCase{
 		$request2 = $this->getRandomRequest();
 
 		// assert neither result is currently in the cache
-		$this->assertFalse( Cache::has( $request1->getHash() ) );
-		$this->assertFalse( Cache::has( $request2->getHash() ) );
+		$this->assertFalse( Cache::has( $request1 ) );
+		$this->assertFalse( Cache::has( $request2 ) );
 
 		// assert the first cache is added correctly
 		Cache::add( $request1 );
-		$this->assertTrue( Cache::has( $request1->getHash() ) );
-		$this->assertEquals( $request1->getResult(), Cache::get( $request1->getHash() ) );
+		$this->assertTrue( Cache::has( $request1 ) );
+		$this->assertEquals( $request1->getResult(), Cache::get( $request1 ) );
 
 		// assert the second cache is added correctly (and the first is still there)
 		Cache::add( $request2 );
-		$this->assertTrue( Cache::has( $request1->getHash() ) );
-		$this->assertEquals( $request1->getResult(), Cache::get( $request1->getHash() ) );
-		$this->assertTrue( Cache::has( $request2->getHash() ) );
-		$this->assertEquals( $request2->getResult(), Cache::get( $request2->getHash() ) );
+		$this->assertTrue( Cache::has( $request1 ) );
+		$this->assertEquals( $request1->getResult(), Cache::get( $request1 ) );
+		$this->assertTrue( Cache::has( $request2 ) );
+		$this->assertEquals( $request2->getResult(), Cache::get( $request2 ) );
 
 		// remove the first result and make sure the second is still there
-		Cache::remove( $request1->getHash() );
-		$this->assertFalse( Cache::has( $request1->getHash() ) );//todo fix removing of cached results
-		$this->assertTrue( Cache::has( $request2->getHash() ) );
-		$this->assertEquals( $request2->getResult(), Cache::get( $request2->getHash() ) );
+		Cache::remove( $request1 );
+		$this->assertFalse( Cache::has( $request1 ) );//todo fix removing of cached results
+		$this->assertTrue( Cache::has( $request2 ) );
+		$this->assertEquals( $request2->getResult(), Cache::get( $request2 ) );
 
 		// clear the cache and assert neither result is there
 		Cache::clear();
-		$this->assertFalse( Cache::has( $request1->getHash() ) );//todo fix removing of cached results
-		$this->assertFalse( Cache::has( $request2->getHash() ) );//todo fix removing of cached results
+		$this->assertFalse( Cache::has( $request1 ) );//todo fix removing of cached results
+		$this->assertFalse( Cache::has( $request2 ) );//todo fix removing of cached results
 
 	}
 
