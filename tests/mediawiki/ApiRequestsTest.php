4fcb95ffabc1870ee7cacd35fe3dc7347d9fa82c
addshore <addshorewiki@gmail.com>
2013-08-24 15:26:25 +0200

    Add basic logout test

--- tests/mediawiki/ApiRequestsTest.php
+++ tests/mediawiki/ApiRequestsTest.php
@@ -1,10 +1,19 @@
 <?php
 
 use Addframe\Mediawiki\Api;
+use Addframe\Mediawiki\LogoutRequest;
 use Addframe\Mediawiki\TokensRequest;
 use Addframe\TestHttp;
 
 class ApiRequestsTest extends PHPUnit_Framework_TestCase{
+
+	function testRequestLogout( ){
+		$expected = '[]';
+		$http = new TestHttp( $expected );
+		$api = new Api( $http );
+		$result = $api->doRequest( new LogoutRequest() );
+		$this->assertEquals( json_decode( $expected, false ), $result );
+	}
 
 	function testRequestToken( ){
 		$expected = '{"tokens":{"edittoken":"+\\"}}';
