a9e96968895e54d64e67cf7ff8aa24a803eb60c0
addshore <addshorewiki@gmail.com>
2013-08-27 17:45:20 +0200

    If we are trying to do tests with no data DONT
    Say that we should probably have defined data if
    we want to get it....

--- tests/mediawiki/MediawikiTestCase.php
+++ tests/mediawiki/MediawikiTestCase.php
@@ -7,10 +7,19 @@ class MediawikiTestCase extends DefaultTestCase {
 
 	/**
 	 * @param $path string of data to get
+	 * @throws UnexpectedValueException
 	 * @return string data from path
 	 */
 	protected function getData( $path ){
-		return file_get_contents( __DIR__.'/data/'.$path );
+		$path =  __DIR__.'/data/'.$path;
+		$data = file_get_contents( $path );
+
+		//If there is no data throw an exception, we should define data!
+		if( is_null( $data ) || $data === false || !is_string( $data ) || empty( $data ) ){
+			throw new UnexpectedValueException( "No data got (you should define it) from {$path}, '{$data}'" );
+		}
+
+		return $data;
 	}
 
 }
