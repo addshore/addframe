b10003b3523940248eae0f755a70ee4809f94433
addshore <addshorewiki@gmail.com>
2013-10-07 18:51:47 +0200

    Load the submodules..

--- Addframe.php
+++ Addframe.php
@@ -18,6 +18,11 @@ spl_autoload_register( function ( $className ) {
 	}
 } );
 
+//todo recursively load entry files for modules rather than defining them here
+include_once __DIR__ . '/modules/WikibaseDataModel/WikibaseDataModel.php';
+include_once __DIR__ . '/modules/DataValues/DataValues.php';
+include_once __DIR__ . '/modules/Diff/Diff.php';
+
 //todo it would be good to also echo the date of the current revision
 echo "Running Addframe, branch '" . GitInfo::currentBranch() . "', commit '" . GitInfo::headSHA1() . "'\n";
 GitInfo::destruct();
