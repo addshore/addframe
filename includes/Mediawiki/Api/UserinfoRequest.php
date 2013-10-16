3c348a1407fca504fcfd2c46e9cdbc600b0ed25b
addshore <addshorewiki@gmail.com>
2013-10-16 13:50:36 +0200

    Turn caching off for userinfo requests..

--- includes/Mediawiki/Api/UserinfoRequest.php
+++ includes/Mediawiki/Api/UserinfoRequest.php
@@ -4,10 +4,11 @@ namespace Addframe\Mediawiki\Api;
 
 /**
  * Class UserinfoRequest
+ * We will never want to cache these as they return information about the currently logged in user
  */
 class UserinfoRequest extends QueryRequest{
 
-	function __construct( $params = array(), $shouldBePosted = false, $maxAge = CACHE_DAY ) {
+	function __construct( $params = array(), $shouldBePosted = false, $maxAge = CACHE_NONE ) {
 
 		$this->addAllowedParams( array( 'meta', 'uiprop' ) );
 		$this->addParams( array( 'meta' => 'userinfo' ) );
