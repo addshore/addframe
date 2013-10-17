b4040d58bd8948d662c92f979f4f3239f825e174
addshore <addshorewiki@gmail.com>
2013-10-17 14:12:26 +0200

    Fix the int test prefix

--- tests/integration/bootstrap.php
+++ tests/integration/bootstrap.php
@@ -17,4 +17,4 @@ require_once( __DIR__ . '/Mediawiki/MediawikiTestCase.php' );
 define( "SITEURL", "http://localhost/wiki/index.php" );
 define( "SITEUSER", "Test" );
 define( "SITEPASS", "integration" );
-define( "PAGEPREFIX", 'AF_' . date( 'Y-m-d H:i:s' ) , '_' );
+define( "PAGEPREFIX", 'AF_' . date( 'Y-m-d H:i:s' ) . '_' );
